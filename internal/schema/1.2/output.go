45444fe212303db29f9f9db6090003ef4758544a
Daniel Banck <daniel@dbanck.de>
2022-08-31 16:02:16 +0200

    Update 1.2 block schemas to function calls

--- internal/schema/1.2/output.go
+++ internal/schema/1.2/output.go
@@ -5,13 +5,15 @@ import (
 	"github.com/hashicorp/hcl-lang/schema"
 )
 
-var outputLifecycleBlock = &schema.BlockSchema{
-	Description: lang.Markdown("Lifecycle customizations, to set a validity condition of the output"),
-	Body: &schema.BodySchema{
-		Blocks: map[string]*schema.BlockSchema{
-			"precondition": {
-				Body: conditionBody,
+func outputLifecycleBlock() *schema.BlockSchema {
+	return &schema.BlockSchema{
+		Description: lang.Markdown("Lifecycle customizations, to set a validity condition of the output"),
+		Body: &schema.BodySchema{
+			Blocks: map[string]*schema.BlockSchema{
+				"precondition": {
+					Body: conditionBody(),
+				},
 			},
 		},
-	},
+	}
 }
