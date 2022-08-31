584489a85322d563275d2fcdac10124458e9ba36
Daniel Banck <daniel@dbanck.de>
2022-08-31 16:02:02 +0200

    Update 0.13 block schemas to function calls

--- internal/schema/0.13/module_block.go
+++ internal/schema/0.13/module_block.go
@@ -9,100 +9,102 @@ import (
 	"github.com/hashicorp/terraform-schema/internal/schema/tokmod"
 )
 
-var moduleBlockSchema = &schema.BlockSchema{
-	Address: &schema.BlockAddrSchema{
-		Steps: []schema.AddrStep{
-			schema.StaticStep{Name: "module"},
-			schema.LabelStep{Index: 0},
-		},
-		FriendlyName: "module",
-		ScopeId:      refscope.ModuleScope,
-		AsReference:  true,
-	},
-	SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Module},
-	Labels: []*schema.LabelSchema{
-		{
-			Name:                   "name",
-			SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name},
-			Description:            lang.PlainText("Reference Name"),
-		},
-	},
-	Description: lang.PlainText("Module block to call a locally or remotely stored module"),
-	Body: &schema.BodySchema{
-		Attributes: map[string]*schema.AttributeSchema{
-			"source": {
-				Expr: schema.LiteralTypeOnly(cty.String),
-				Description: lang.Markdown("Source where to load the module from, " +
-					"a local directory (e.g. `./module`) or a remote address - e.g. " +
-					"`hashicorp/consul/aws` (Terraform Registry address) or " +
-					"`github.com/hashicorp/example` (GitHub)"),
-				IsRequired:             true,
-				IsDepKey:               true,
-				SemanticTokenModifiers: lang.SemanticTokenModifiers{lang.TokenModifierDependent},
-				CompletionHooks: lang.CompletionHooks{
-					{
-						Name: "CompleteLocalModuleSources",
-					},
-					{
-						Name: "CompleteRegistryModuleSources",
-					},
-				},
+func moduleBlockSchema() *schema.BlockSchema {
+	return &schema.BlockSchema{
+		Address: &schema.BlockAddrSchema{
+			Steps: []schema.AddrStep{
+				schema.StaticStep{Name: "module"},
+				schema.LabelStep{Index: 0},
 			},
-			"version": {
-				Expr:       schema.LiteralTypeOnly(cty.String),
-				IsOptional: true,
-				Description: lang.Markdown("Constraint to set the version of the module, e.g. `~> 1.0`." +
-					" Only applicable to modules in a module registry."),
-				CompletionHooks: lang.CompletionHooks{
-					{
-						Name: "CompleteRegistryModuleVersions",
-					},
-				},
+			FriendlyName: "module",
+			ScopeId:      refscope.ModuleScope,
+			AsReference:  true,
+		},
+		SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Module},
+		Labels: []*schema.LabelSchema{
+			{
+				Name:                   "name",
+				SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name},
+				Description:            lang.PlainText("Reference Name"),
 			},
-			"providers": {
-				Expr: schema.ExprConstraints{
-					schema.MapExpr{
-						Name: "map of provider references",
-						Elem: schema.ExprConstraints{
-							schema.TraversalExpr{OfScopeId: refscope.ProviderScope},
+		},
+		Description: lang.PlainText("Module block to call a locally or remotely stored module"),
+		Body: &schema.BodySchema{
+			Attributes: map[string]*schema.AttributeSchema{
+				"source": {
+					Expr: schema.LiteralTypeOnly(cty.String),
+					Description: lang.Markdown("Source where to load the module from, " +
+						"a local directory (e.g. `./module`) or a remote address - e.g. " +
+						"`hashicorp/consul/aws` (Terraform Registry address) or " +
+						"`github.com/hashicorp/example` (GitHub)"),
+					IsRequired:             true,
+					IsDepKey:               true,
+					SemanticTokenModifiers: lang.SemanticTokenModifiers{lang.TokenModifierDependent},
+					CompletionHooks: lang.CompletionHooks{
+						{
+							Name: "CompleteLocalModuleSources",
+						},
+						{
+							Name: "CompleteRegistryModuleSources",
 						},
 					},
 				},
-				IsOptional:  true,
-				Description: lang.Markdown("Explicit mapping of providers which the module uses"),
-			},
-			"count": {
-				Expr: schema.ExprConstraints{
-					schema.TraversalExpr{OfType: cty.Number},
-					schema.LiteralTypeExpr{Type: cty.Number},
-				},
-				IsOptional:  true,
-				Description: lang.Markdown("Number of instances of this module, e.g. `3`"),
-			},
-			"for_each": {
-				Expr: schema.ExprConstraints{
-					schema.TraversalExpr{OfType: cty.Set(cty.DynamicPseudoType)},
-					schema.TraversalExpr{OfType: cty.Map(cty.DynamicPseudoType)},
-					schema.LiteralTypeExpr{Type: cty.Set(cty.DynamicPseudoType)},
-					schema.LiteralTypeExpr{Type: cty.Map(cty.DynamicPseudoType)},
-				},
-				IsOptional:  true,
-				Description: lang.Markdown("A set or a map where each item represents an instance of this module"),
-			},
-			"depends_on": {
-				Expr: schema.ExprConstraints{
-					schema.TupleConsExpr{
-						Name: "set of references",
-						AnyElem: schema.ExprConstraints{
-							schema.TraversalExpr{OfScopeId: refscope.DataScope},
-							schema.TraversalExpr{OfScopeId: refscope.ModuleScope},
-							schema.TraversalExpr{OfScopeId: refscope.ResourceScope},
+				"version": {
+					Expr:       schema.LiteralTypeOnly(cty.String),
+					IsOptional: true,
+					Description: lang.Markdown("Constraint to set the version of the module, e.g. `~> 1.0`." +
+						" Only applicable to modules in a module registry."),
+					CompletionHooks: lang.CompletionHooks{
+						{
+							Name: "CompleteRegistryModuleVersions",
 						},
 					},
 				},
-				IsOptional:  true,
-				Description: lang.Markdown("Set of references to hidden dependencies, e.g. other resources or data sources"),
+				"providers": {
+					Expr: schema.ExprConstraints{
+						schema.MapExpr{
+							Name: "map of provider references",
+							Elem: schema.ExprConstraints{
+								schema.TraversalExpr{OfScopeId: refscope.ProviderScope},
+							},
+						},
+					},
+					IsOptional:  true,
+					Description: lang.Markdown("Explicit mapping of providers which the module uses"),
+				},
+				"count": {
+					Expr: schema.ExprConstraints{
+						schema.TraversalExpr{OfType: cty.Number},
+						schema.LiteralTypeExpr{Type: cty.Number},
+					},
+					IsOptional:  true,
+					Description: lang.Markdown("Number of instances of this module, e.g. `3`"),
+				},
+				"for_each": {
+					Expr: schema.ExprConstraints{
+						schema.TraversalExpr{OfType: cty.Set(cty.DynamicPseudoType)},
+						schema.TraversalExpr{OfType: cty.Map(cty.DynamicPseudoType)},
+						schema.LiteralTypeExpr{Type: cty.Set(cty.DynamicPseudoType)},
+						schema.LiteralTypeExpr{Type: cty.Map(cty.DynamicPseudoType)},
+					},
+					IsOptional:  true,
+					Description: lang.Markdown("A set or a map where each item represents an instance of this module"),
+				},
+				"depends_on": {
+					Expr: schema.ExprConstraints{
+						schema.TupleConsExpr{
+							Name: "set of references",
+							AnyElem: schema.ExprConstraints{
+								schema.TraversalExpr{OfScopeId: refscope.DataScope},
+								schema.TraversalExpr{OfScopeId: refscope.ModuleScope},
+								schema.TraversalExpr{OfScopeId: refscope.ResourceScope},
+							},
+						},
+					},
+					IsOptional:  true,
+					Description: lang.Markdown("Set of references to hidden dependencies, e.g. other resources or data sources"),
+				},
 			},
 		},
-	},
+	}
 }
--- internal/schema/0.13/provider_block.go
+++ internal/schema/0.13/provider_block.go
@@ -9,41 +9,43 @@ import (
 	"github.com/hashicorp/terraform-schema/internal/schema/tokmod"
 )
 
-var providerBlockSchema = &schema.BlockSchema{
-	Address: &schema.BlockAddrSchema{
-		Steps: []schema.AddrStep{
-			schema.LabelStep{Index: 0},
-			schema.AttrValueStep{Name: "alias", IsOptional: true},
-		},
-		FriendlyName: "provider",
-		ScopeId:      refscope.ProviderScope,
-		AsReference:  true,
-	},
-	SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Provider},
-	Labels: []*schema.LabelSchema{
-		{
-			Name:                   "name",
-			SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name, lang.TokenModifierDependent},
-			Description:            lang.PlainText("Provider Name"),
-			IsDepKey:               true,
-			Completable:            true,
-		},
-	},
-	Description: lang.PlainText("A provider block is used to specify a provider configuration"),
-	Body: &schema.BodySchema{
-		Attributes: map[string]*schema.AttributeSchema{
-			"alias": {
-				Expr:        schema.LiteralTypeOnly(cty.String),
-				IsOptional:  true,
-				Description: lang.Markdown("Alias for using the same provider with different configurations for different resources, e.g. `eu-west`"),
+func providerBlockSchema() *schema.BlockSchema {
+	return &schema.BlockSchema{
+		Address: &schema.BlockAddrSchema{
+			Steps: []schema.AddrStep{
+				schema.LabelStep{Index: 0},
+				schema.AttrValueStep{Name: "alias", IsOptional: true},
 			},
-			"version": {
-				Expr:         schema.LiteralTypeOnly(cty.String),
-				IsOptional:   true,
-				IsDeprecated: true,
-				Description: lang.Markdown("Specifies a version constraint for the provider. e.g. `~> 1.0`.\n" +
-					"**DEPRECATED:** Use `required_providers` block to manage provider version instead."),
+			FriendlyName: "provider",
+			ScopeId:      refscope.ProviderScope,
+			AsReference:  true,
+		},
+		SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Provider},
+		Labels: []*schema.LabelSchema{
+			{
+				Name:                   "name",
+				SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name, lang.TokenModifierDependent},
+				Description:            lang.PlainText("Provider Name"),
+				IsDepKey:               true,
+				Completable:            true,
 			},
 		},
-	},
+		Description: lang.PlainText("A provider block is used to specify a provider configuration"),
+		Body: &schema.BodySchema{
+			Attributes: map[string]*schema.AttributeSchema{
+				"alias": {
+					Expr:        schema.LiteralTypeOnly(cty.String),
+					IsOptional:  true,
+					Description: lang.Markdown("Alias for using the same provider with different configurations for different resources, e.g. `eu-west`"),
+				},
+				"version": {
+					Expr:         schema.LiteralTypeOnly(cty.String),
+					IsOptional:   true,
+					IsDeprecated: true,
+					Description: lang.Markdown("Specifies a version constraint for the provider. e.g. `~> 1.0`.\n" +
+						"**DEPRECATED:** Use `required_providers` block to manage provider version instead."),
+				},
+			},
+		},
+	}
 }
