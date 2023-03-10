9019fdc7a59a6c8742a66ee41503d1a486312cc2
James Pogran <jpogran@outlook.com>
2023-03-10 10:02:26 -0500

    Add Cloud block equality method (#196)

--- backend/cloud.go
+++ backend/cloud.go
@@ -6,3 +6,19 @@ package backend
 type Cloud struct {
 	Hostname string
 }
+
+func (be *Cloud) Equals(b *Cloud) bool {
+	if be == nil && b == nil {
+		return true
+	}
+
+	if be == nil || b == nil {
+		return false
+	}
+
+	if be.Hostname != b.Hostname {
+		return false
+	}
+
+	return true
+}
