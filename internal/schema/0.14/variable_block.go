c6d9e30ee449f0618ace6c4dce34bff9469d66bb
Daniel Banck <daniel@dbanck.de>
2023-09-27 09:53:59 +0200

    Add `nullable` to `variable` blocks for 1.1+

--- internal/schema/0.14/variable_block.go
+++ internal/schema/0.14/variable_block.go
@@ -12,68 +12,70 @@ import (
 	"github.com/hashicorp/terraform-schema/internal/schema/tokmod"
 )
 
-var variableBlockSchema = &schema.BlockSchema{
-	Address: &schema.BlockAddrSchema{
-		Steps: []schema.AddrStep{
-			schema.StaticStep{Name: "var"},
-			schema.LabelStep{Index: 0},
-		},
-		FriendlyName: "variable",
-		ScopeId:      refscope.VariableScope,
-		AsReference:  true,
-		AsTypeOf: &schema.BlockAsTypeOf{
-			AttributeExpr:  "type",
-			AttributeValue: "default",
-		},
-	},
-	SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Variable},
-	Labels: []*schema.LabelSchema{
-		{
-			Name:                   "name",
-			SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name},
-			Description:            lang.PlainText("Variable Name"),
-		},
-	},
-	Description: lang.Markdown("Input variable allowing users to customizate aspects of the configuration when used directly " +
-		"(e.g. via CLI, `tfvars` file or via environment variables), or as a module (via `module` arguments)"),
-	Body: &schema.BodySchema{
-		Attributes: map[string]*schema.AttributeSchema{
-			"description": {
-				Constraint:  schema.LiteralType{Type: cty.String},
-				IsOptional:  true,
-				Description: lang.Markdown("Description to document the purpose of the variable and what value is expected"),
+func variableBlockSchema() *schema.BlockSchema {
+	return &schema.BlockSchema{
+		Address: &schema.BlockAddrSchema{
+			Steps: []schema.AddrStep{
+				schema.StaticStep{Name: "var"},
+				schema.LabelStep{Index: 0},
 			},
-			"type": {
-				Constraint:  schema.TypeDeclaration{},
-				IsOptional:  true,
-				Description: lang.Markdown("Type constraint restricting the type of value to accept, e.g. `string` or `list(string)`"),
-			},
-			"sensitive": {
-				Constraint:  schema.LiteralType{Type: cty.Bool},
-				IsOptional:  true,
-				Description: lang.Markdown("Whether the variable contains sensitive material and should be hidden in the UI"),
+			FriendlyName: "variable",
+			ScopeId:      refscope.VariableScope,
+			AsReference:  true,
+			AsTypeOf: &schema.BlockAsTypeOf{
+				AttributeExpr:  "type",
+				AttributeValue: "default",
 			},
 		},
-		Blocks: map[string]*schema.BlockSchema{
-			"validation": {
-				Description: lang.Markdown("Custom validation rule to restrict what value is expected for the variable"),
-				Body: &schema.BodySchema{
-					Attributes: map[string]*schema.AttributeSchema{
-						"condition": {
-							Constraint: schema.LiteralType{Type: cty.Bool},
-							IsRequired: true,
-							Description: lang.Markdown("Condition under which a variable value is valid, " +
-								"e.g. `length(var.example) >= 4` enforces minimum of 4 characters"),
-						},
-						"error_message": {
-							Constraint: schema.LiteralType{Type: cty.String},
-							IsRequired: true,
-							Description: lang.Markdown("Error message to present when the variable is considered invalid, " +
-								"i.e. when `condition` evaluates to `false`"),
+		SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Variable},
+		Labels: []*schema.LabelSchema{
+			{
+				Name:                   "name",
+				SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name},
+				Description:            lang.PlainText("Variable Name"),
+			},
+		},
+		Description: lang.Markdown("Input variable allowing users to customizate aspects of the configuration when used directly " +
+			"(e.g. via CLI, `tfvars` file or via environment variables), or as a module (via `module` arguments)"),
+		Body: &schema.BodySchema{
+			Attributes: map[string]*schema.AttributeSchema{
+				"description": {
+					Constraint:  schema.LiteralType{Type: cty.String},
+					IsOptional:  true,
+					Description: lang.Markdown("Description to document the purpose of the variable and what value is expected"),
+				},
+				"type": {
+					Constraint:  schema.TypeDeclaration{},
+					IsOptional:  true,
+					Description: lang.Markdown("Type constraint restricting the type of value to accept, e.g. `string` or `list(string)`"),
+				},
+				"sensitive": {
+					Constraint:  schema.LiteralType{Type: cty.Bool},
+					IsOptional:  true,
+					Description: lang.Markdown("Whether the variable contains sensitive material and should be hidden in the UI"),
+				},
+			},
+			Blocks: map[string]*schema.BlockSchema{
+				"validation": {
+					Description: lang.Markdown("Custom validation rule to restrict what value is expected for the variable"),
+					Body: &schema.BodySchema{
+						Attributes: map[string]*schema.AttributeSchema{
+							"condition": {
+								Constraint: schema.LiteralType{Type: cty.Bool},
+								IsRequired: true,
+								Description: lang.Markdown("Condition under which a variable value is valid, " +
+									"e.g. `length(var.example) >= 4` enforces minimum of 4 characters"),
+							},
+							"error_message": {
+								Constraint: schema.LiteralType{Type: cty.String},
+								IsRequired: true,
+								Description: lang.Markdown("Error message to present when the variable is considered invalid, " +
+									"i.e. when `condition` evaluates to `false`"),
+							},
 						},
 					},
 				},
 			},
 		},
-	},
+	}
 }
