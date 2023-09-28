e652b8d8b4d66a0b9683b82d9e2a1212e4dc1a36
Radek Simko <radek.simko@gmail.com>
2023-09-28 13:16:16 +0100

    backends: Update s3 backend w/ v1.6 changes

--- internal/schema/backends/s3.go
+++ internal/schema/backends/s3.go
@@ -244,6 +244,238 @@ func s3Backend(v *version.Version) *schema.BodySchema {
 			Description: lang.Markdown("Assume role session tag keys to pass to any subsequent sessions."),
 		}
 	}
+	if v.GreaterThanOrEqual(v1_6_0) {
+		bodySchema.Attributes["region"].IsRequired = false
+		bodySchema.Attributes["region"].IsOptional = true
+
+		// deprecations
+		bodySchema.Attributes["assume_role_duration_seconds"].IsDeprecated = true
+		bodySchema.Attributes["assume_role_policy_arns"].IsDeprecated = true
+		bodySchema.Attributes["assume_role_policy"].IsDeprecated = true
+		bodySchema.Attributes["assume_role_tags"].IsDeprecated = true
+		bodySchema.Attributes["assume_role_transitive_tag_keys"].IsDeprecated = true
+		bodySchema.Attributes["dynamodb_endpoint"].IsDeprecated = true
+		bodySchema.Attributes["endpoint"].IsDeprecated = true
+		bodySchema.Attributes["external_id"].IsDeprecated = true
+		bodySchema.Attributes["force_path_style"].IsDeprecated = true
+		bodySchema.Attributes["iam_endpoint"].IsDeprecated = true
+		bodySchema.Attributes["role_arn"].IsDeprecated = true
+		bodySchema.Attributes["session_name"].IsDeprecated = true
+		bodySchema.Attributes["shared_credentials_file"].IsDeprecated = true
+		bodySchema.Attributes["sts_endpoint"].IsDeprecated = true
+
+		// new fields
+		bodySchema.Attributes["allowed_account_ids"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+			IsOptional:  true,
+			Description: lang.Markdown("List of allowed AWS account IDs."),
+		}
+		bodySchema.Attributes["ec2_metadata_service_endpoint"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("Address of the EC2 metadata service (IMDS) endpoint to use."),
+		}
+		bodySchema.Attributes["ec2_metadata_service_endpoint_mode"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("Mode to use in communicating with the metadata service."),
+		}
+		bodySchema.Attributes["endpoints"] = &schema.AttributeSchema{
+			Constraint: schema.Object{
+				Attributes: schema.ObjectAttributes{
+					"dynamodb": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("A custom endpoint for the DynamoDB API"),
+					},
+
+					"iam": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("A custom endpoint for the IAM API"),
+					},
+
+					"s3": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("A custom endpoint for the S3 API"),
+					},
+
+					"sts": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("A custom endpoint for the STS API"),
+					},
+				},
+			},
+		}
+		bodySchema.Attributes["forbidden_account_ids"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+			IsOptional:  true,
+			Description: lang.Markdown("List of forbidden AWS account IDs."),
+		}
+		bodySchema.Attributes["sts_region"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("AWS region for STS."),
+		}
+		bodySchema.Attributes["retry_mode"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("Specifies how retries are attempted."),
+		}
+		bodySchema.Attributes["shared_config_files"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+			IsOptional:  true,
+			Description: lang.Markdown("List of paths to shared config files"),
+		}
+		bodySchema.Attributes["shared_credentials_files"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+			IsOptional:  true,
+			Description: lang.Markdown("List of paths to shared credentials files"),
+		}
+		bodySchema.Attributes["use_path_style"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Bool},
+			IsOptional:  true,
+			Description: lang.Markdown("Enable path-style S3 URLs."),
+		}
+		bodySchema.Attributes["assume_role"] = &schema.AttributeSchema{
+			Constraint: schema.Object{
+				Attributes: schema.ObjectAttributes{
+					"role_arn": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsRequired:  true,
+						Description: lang.Markdown("The role to be assumed."),
+					},
+
+					"duration": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("The duration, between 15 minutes and 12 hours, of the role session. Valid time units are ns, us (or µs), ms, s, h, or m."),
+					},
+
+					"external_id": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("The external ID to use when assuming the role"),
+					},
+
+					"policy": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("IAM Policy JSON describing further restricting permissions for the IAM Role being assumed."),
+					},
+
+					"policy_arns": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+						IsOptional:  true,
+						Description: lang.Markdown("Amazon Resource Names (ARNs) of IAM Policies describing further restricting permissions for the IAM Role being assumed."),
+					},
+
+					"session_name": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("The session name to use when assuming the role."),
+					},
+
+					"source_identity": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("Source identity specified by the principal assuming the role."),
+					},
+
+					"tags": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.Map(cty.String)},
+						IsOptional:  true,
+						Description: lang.Markdown("Assume role session tags."),
+					},
+
+					"transitive_tag_keys": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+						IsOptional:  true,
+						Description: lang.Markdown("Assume role session tag keys to pass to any subsequent sessions."),
+					},
+				},
+			},
+		}
+		bodySchema.Attributes["assume_role_with_web_identity"] = &schema.AttributeSchema{
+			Constraint: schema.Object{
+				Attributes: schema.ObjectAttributes{
+					"role_arn": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsRequired:  true,
+						Description: lang.Markdown("The role to be assumed."),
+					},
+
+					"duration": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("The duration, between 15 minutes and 12 hours, of the role session. Valid time units are ns, us (or µs), ms, s, h, or m."),
+					},
+
+					"policy": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("IAM Policy JSON describing further restricting permissions for the IAM Role being assumed."),
+					},
+
+					"policy_arns": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.Set(cty.String)},
+						IsOptional:  true,
+						Description: lang.Markdown("Amazon Resource Names (ARNs) of IAM Policies describing further restricting permissions for the IAM Role being assumed."),
+					},
+
+					"session_name": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("The session name to use when assuming the role."),
+					},
+
+					"web_identity_token": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("Value of a web identity token from an OpenID Connect (OIDC) or OAuth provider."),
+					},
+
+					"web_identity_token_file": &schema.AttributeSchema{
+						Constraint:  schema.LiteralType{Type: cty.String},
+						IsOptional:  true,
+						Description: lang.Markdown("File containing a web identity token from an OpenID Connect (OIDC) or OAuth provider."),
+					},
+				},
+			},
+		}
+		bodySchema.Attributes["use_legacy_workflow"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Bool},
+			IsOptional:  true,
+			Description: lang.Markdown("Use the legacy authentication workflow, preferring environment variables over backend configuration."),
+		}
+		bodySchema.Attributes["custom_ca_bundle"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("File containing custom root and intermediate certificates."),
+		}
+		bodySchema.Attributes["http_proxy"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+			Description: lang.Markdown("Address of an HTTP proxy to use when accessing the AWS API."),
+		}
+		bodySchema.Attributes["insecure"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Bool},
+			IsOptional:  true,
+			Description: lang.Markdown("Whether to explicitly allow the backend to perform insecure SSL requests."),
+		}
+		bodySchema.Attributes["use_fips_endpoint"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Bool},
+			IsOptional:  true,
+			Description: lang.Markdown("Force the backend to resolve endpoints with FIPS capability."),
+		}
+		bodySchema.Attributes["use_dualstack_endpoint"] = &schema.AttributeSchema{
+			Constraint:  schema.LiteralType{Type: cty.Bool},
+			IsOptional:  true,
+			Description: lang.Markdown("Force the backend to resolve endpoints with DualStack capability."),
+		}
+	}
 
 	return bodySchema
 }
