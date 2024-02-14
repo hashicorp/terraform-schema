d589967d40f453b234b5bc76f91b2cbc8b409baa
Ansgar Mertens <ansgar@hashicorp.com>
2024-02-14 16:52:20 +0100

    tests: add more tests for functions_merge.go

--- schema/functions_merge_test.go
+++ schema/functions_merge_test.go
@@ -6,9 +6,20 @@ package schema
 import (
 	"errors"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hcl-lang/schema"
+	tfjson "github.com/hashicorp/terraform-json"
+	tfaddr "github.com/hashicorp/terraform-registry-address"
+	"github.com/hashicorp/terraform-schema/internal/addr"
+	tfmod "github.com/hashicorp/terraform-schema/module"
+	"github.com/zclconf/go-cty-debug/ctydebug"
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/function"
 )
 
-func TestSchemaMerger_FunctionsForModule_noCoreFunctions(t *testing.T) {
+func TestFunctionsMerger_FunctionsForModule_noCoreFunctions(t *testing.T) {
 	sm := NewFunctionsMerger(nil)
 
 	_, err := sm.FunctionsForModule(nil)
@@ -21,4 +32,101 @@ func TestSchemaMerger_FunctionsForModule_noCoreFunctions(t *testing.T) {
 	}
 }
 
-// TODO: add more tests, like in schema_merge_test.go
+func TestFunctionsMerger_FunctionsForModule_noMeta(t *testing.T) {
+	coreFunctions := map[string]schema.FunctionSignature{
+		"foo": {
+			Params: []function.Parameter{
+				{Name: "bar", Type: cty.String, Description: "bar function"},
+			},
+		},
+	}
+
+	sm := NewFunctionsMerger(coreFunctions)
+
+	functions, err := sm.FunctionsForModule(nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %#v", err)
+	}
+
+	if len(functions) != 1 {
+		t.Fatalf("unexpected functions: %#v", functions)
+	}
+
+	if functions["foo"].Params[0].Name != "bar" {
+		t.Fatalf("unexpected function: %#v", functions)
+	}
+}
+
+func TestFunctionsMerger_FunctionsForModule(t *testing.T) {
+	coreFunctions := map[string]schema.FunctionSignature{
+		"foo": {
+			Params: []function.Parameter{
+				{Name: "bar", Type: cty.String, Description: "bar param"},
+			},
+			Description: "foo function",
+		},
+	}
+
+	fm := NewFunctionsMerger(coreFunctions)
+	fm.SetSchemaReader(&testJsonSchemaReader{
+		ps: &tfjson.ProviderSchemas{
+			FormatVersion: "1.0",
+			Schemas: map[string]*tfjson.ProviderSchema{
+				"registry.terraform.io/hashicorp/test": {
+					ConfigSchema:      &tfjson.Schema{},
+					DataSourceSchemas: map[string]*tfjson.Schema{},
+					ResourceSchemas:   map[string]*tfjson.Schema{},
+					Functions: map[string]*tfjson.FunctionSignature{
+						"bar": {
+							Parameters: []*tfjson.FunctionParameter{
+								{Name: "baz", Type: cty.String, Description: "baz param"},
+							},
+							Description: "bar function",
+						},
+					},
+				},
+			},
+		},
+	})
+
+	testProvider := addr.NewDefaultProvider("test")
+	versionConstraints, err := version.NewConstraint("1.0.0")
+
+	if err != nil {
+		t.Fatalf("unexpected error: %#v", err)
+	}
+
+	meta := &tfmod.Meta{
+		ProviderReferences: map[tfmod.ProviderRef]tfaddr.Provider{
+			{LocalName: "localtest"}: testProvider,
+		},
+		ProviderRequirements: tfmod.ProviderRequirements{
+			testProvider: versionConstraints,
+		},
+	}
+
+	expectedFunctions := map[string]schema.FunctionSignature{
+		"foo": {
+			Params: []function.Parameter{
+				{Name: "bar", Type: cty.String, Description: "bar param"},
+			},
+			Description: "foo function",
+		},
+		"provider::localtest::bar": {
+			Params: []function.Parameter{
+				{Name: "baz", Type: cty.String, Description: "baz param"},
+			},
+			Description: "bar function",
+			Detail:      "hashicorp/test",
+		},
+	}
+
+	givenFunctions, err := fm.FunctionsForModule(meta)
+	if err != nil {
+		t.Fatalf("unexpected error: %#v", err)
+	}
+
+	if diff := cmp.Diff(expectedFunctions, givenFunctions, ctydebug.CmpOptions); diff != "" {
+		t.Fatalf("functions mismatch: %s", diff)
+	}
+}
