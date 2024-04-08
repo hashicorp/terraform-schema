5d79071e53802dd1a3f948571b5408d7ae8138ea
Ansgar Mertens <ansgar@hashicorp.com>
2024-04-08 15:50:35 +0200

    rename: Rename TFC to HCP Terraform

--- internal/schema/1.6/terraform.go
+++ internal/schema/1.6/terraform.go
@@ -15,21 +15,21 @@ func patchTerraformBlockSchema(bs *schema.BlockSchema) *schema.BlockSchema {
 			"name": {
 				Constraint: schema.LiteralType{Type: cty.String},
 				IsOptional: true,
-				Description: lang.Markdown("The name of a single Terraform Cloud workspace " +
+				Description: lang.Markdown("The name of a single HCP Terraform workspace " +
 					"to be used with this configuration. When configured only the specified workspace " +
 					"can be used. This option conflicts with `tags`."),
 			},
 			"project": {
 				Constraint:  schema.LiteralType{Type: cty.String},
 				IsOptional:  true,
-				Description: lang.PlainText("The name of a Terraform Cloud project. Workspaces that need creating will be created within this project."),
+				Description: lang.PlainText("The name of a HCP Terraform project. Workspaces that need creating will be created within this project."),
 			},
 			"tags": {
 				Constraint: schema.Set{
 					Elem: schema.LiteralType{Type: cty.String},
 				},
 				IsOptional: true,
-				Description: lang.Markdown("A set of tags used to select remote Terraform Cloud workspaces" +
+				Description: lang.Markdown("A set of tags used to select remote HCP Terraform workspaces" +
 					" to be used for this single configuration. New workspaces will automatically be tagged " +
 					"with these tag values. Generally, this is the primary and recommended strategy to use. " +
 					"This option conflicts with `name`."),
