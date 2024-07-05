1bd8708bfa14488e58b3a05dc38806944a482169
Ansgar Mertens <ansgar@hashicorp.com>
2024-07-05 14:41:46 +0200

    chore: change TF version to 1.9 for stacks schema

--- schema/stacks_schema.go
+++ schema/stacks_schema.go
@@ -3,19 +3,19 @@ package schema
 import (
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/hcl-lang/schema"
-	stack_1_8 "github.com/hashicorp/terraform-schema/internal/schema/stacks/1.8"
+	stack_1_9 "github.com/hashicorp/terraform-schema/internal/schema/stacks/1.9"
 )
 
 // CoreStackSchemaForVersion finds a schema for stack configuration files
 // that is relevant for the given Terraform version.
 // It will return an error if such schema cannot be found.
 func CoreStackSchemaForVersion(v *version.Version) (*schema.BodySchema, error) {
-	return stack_1_8.StackSchema(v), nil
+	return stack_1_9.StackSchema(v), nil
 }
 
 // CoreDeploySchemaForVersion finds a schema for deployment configuration files
 // that is relevant for the given Terraform version.
 // It will return an error if such schema cannot be found.
 func CoreDeploySchemaForVersion(v *version.Version) (*schema.BodySchema, error) {
-	return stack_1_8.DeploymentSchema(v), nil
+	return stack_1_9.DeploymentSchema(v), nil
 }
