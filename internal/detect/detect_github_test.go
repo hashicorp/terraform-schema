7b54ec2ee30f67426ccdb444f52ef2439af7008c
Daniel Banck <daniel@dbanck.de>
2024-07-11 11:23:04 +0200

    Introduce detect pkg for detecting remote sources
    It's based on same go-getter detection logic that Terraform uses to
    identify module source addresses.

---
+++ internal/detect/detect_github_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package detect
+
+import (
+	"testing"
+)
+
+func TestGitHubDetector(t *testing.T) {
+	cases := []struct {
+		Input  string
+		Output string
+	}{
+		// HTTP
+		{"github.com/hashicorp/foo", "git::https://github.com/hashicorp/foo.git"},
+		{"github.com/hashicorp/foo.git", "git::https://github.com/hashicorp/foo.git"},
+		{
+			"github.com/hashicorp/foo/bar",
+			"git::https://github.com/hashicorp/foo.git//bar",
+		},
+		{
+			"github.com/hashicorp/foo?foo=bar",
+			"git::https://github.com/hashicorp/foo.git?foo=bar",
+		},
+		{
+			"github.com/hashicorp/foo.git?foo=bar",
+			"git::https://github.com/hashicorp/foo.git?foo=bar",
+		},
+	}
+
+	f := new(GitHubDetector)
+	for i, tc := range cases {
+		output, ok, err := f.Detect(tc.Input)
+		if err != nil {
+			t.Fatalf("err: %s", err)
+		}
+		if !ok {
+			t.Fatal("not ok")
+		}
+
+		if output != tc.Output {
+			t.Fatalf("%d: bad: %#v", i, output)
+		}
+	}
+}
---
+++ internal/detect/detect_test.go
@@ -0,0 +1,71 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package detect
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDetect(t *testing.T) {
+	cases := []struct {
+		Input  string
+		Output string
+		Err    bool
+	}{
+		{
+
+			"git::github.com/hashicorp/foo",
+			"git::https://github.com/hashicorp/foo.git",
+			false,
+		},
+		{
+			"git::github.com/hashicorp/foo//bar",
+			"git::https://github.com/hashicorp/foo.git//bar",
+			false,
+		},
+		{
+			"git::https://github.com/hashicorp/consul.git",
+			"git::https://github.com/hashicorp/consul.git",
+			false,
+		},
+		{
+			"git::https://person@someothergit.com/foo/bar",
+			"git::https://person@someothergit.com/foo/bar",
+			false,
+		},
+		{
+			"git::https://person@someothergit.com/foo/bar",
+			"git::https://person@someothergit.com/foo/bar",
+			false,
+		},
+		{
+			"git::ssh://git@my.custom.git/dir1/dir2",
+			"git::ssh://git@my.custom.git/dir1/dir2",
+			false,
+		},
+		{
+			"git::git@my.custom.git:dir1/dir2",
+			"git::ssh://git@my.custom.git/dir1/dir2",
+			false,
+		},
+		{
+			"git::git@my.custom.git:dir1/dir2",
+			"git::ssh://git@my.custom.git/dir1/dir2",
+			false,
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d %s", i, tc.Input), func(t *testing.T) {
+			output, err := Detect(tc.Input, RemoteSourceDetectors)
+			if err != nil != tc.Err {
+				t.Fatalf("%d: bad err: %s", i, err)
+			}
+			if output != tc.Output {
+				t.Fatalf("%d: bad output: %s\nexpected: %s", i, output, tc.Output)
+			}
+		})
+	}
+}
