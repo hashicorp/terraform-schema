6a86555222df1aff115694b48613d6d412c81a7a
Ansgar Mertens <ansgar@hashicorp.com>
2024-07-23 15:50:47 +0200

    feat: support setting a module reader on stacks schema merger

--- schema/stacks/stack_schema_merge.go
+++ schema/stacks/stack_schema_merge.go
@@ -4,17 +4,22 @@
 package schema
 
 import (
+	"path/filepath"
+
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/schema"
 	tfaddr "github.com/hashicorp/terraform-registry-address"
+	tfmod "github.com/hashicorp/terraform-schema/module"
 	tfschema "github.com/hashicorp/terraform-schema/schema"
 	"github.com/hashicorp/terraform-schema/stack"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type StackSchemaMerger struct {
-	coreSchema  *schema.BodySchema
-	stateReader StateReader
+	coreSchema   *schema.BodySchema
+	stateReader  StateReader
+	moduleReader ModuleReader
 }
 
 // StateReader exposes a set of methods to read data from the internal language server state
@@ -22,6 +27,12 @@ type StateReader interface {
 	// ProviderSchema returns the schema for a provider we have stored in memory. The can come
 	// from different sources.
 	ProviderSchema(modPath string, addr tfaddr.Provider, vc version.Constraints) (*tfschema.ProviderSchema, error)
+}
+
+type ModuleReader interface {
+	// LocalModuleMeta returns the module meta data for a local module. This is the result
+	// of the [earlydecoder] when processing module files
+	LocalModuleMeta(modPath string) (*tfmod.Meta, error)
 }
 
 func NewStackSchemaMerger(coreSchema *schema.BodySchema) *StackSchemaMerger {
@@ -32,6 +43,10 @@ func NewStackSchemaMerger(coreSchema *schema.BodySchema) *StackSchemaMerger {
 
 func (m *StackSchemaMerger) SetStateReader(mr StateReader) {
 	m.stateReader = mr
+}
+
+func (m *StackSchemaMerger) SetModuleReader(mr ModuleReader) {
+	m.moduleReader = mr
 }
 
 func (m *StackSchemaMerger) SchemaForModule(meta *stack.Meta) (*schema.BodySchema, error) {
@@ -90,6 +105,31 @@ func (m *StackSchemaMerger) SchemaForModule(meta *stack.Meta) (*schema.BodySchem
 		mergedSchema.Blocks["variable"].DependentBody = variableDependentBody(meta.Variables)
 	}
 
+	for _, comp := range meta.Components {
+		depKeys := schema.DependencyKeys{
+			Attributes: []schema.AttributeDependent{
+				{
+					Name: "source",
+					Expr: schema.ExpressionValue{
+						Static: cty.StringVal(comp.Source),
+					},
+				},
+			},
+		}
+		switch sourceAddr := comp.SourceAddr.(type) {
+		case tfmod.LocalSourceAddr:
+			path := filepath.Join(meta.Path, sourceAddr.String())
+
+			modMeta, err := m.moduleReader.LocalModuleMeta(path)
+			if err == nil {
+				depSchema, err := schemaForDependentComponentBlock(comp, modMeta)
+				if err == nil {
+					mergedSchema.Blocks["component"].DependentBody[schema.NewSchemaKey(depKeys)] = depSchema
+				}
+			}
+		}
+	}
+
 	return mergedSchema, nil
 }
 
@@ -114,4 +154,11 @@ func variableDependentBody(vars map[string]stack.Variable) map[schema.SchemaKey]
 	}
 
 	return depBodies
+}
+
+// TODO: move to extra file? (there's an extra file in the original code in modules)
+func schemaForDependentComponentBlock(component stack.Component, modMeta *tfmod.Meta) (*schema.BodySchema, error) {
+	// TODO: implement
+
+	return nil, nil
 }
