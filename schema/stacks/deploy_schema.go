9fc1ce56945b83e16fce4f6b516634001275e03d
Ansgar Mertens <ansgar@hashicorp.com>
2024-07-30 14:30:44 +0200

    feat: add schema merger for stacks (#376)
    * chore: make provider schema available in schema merger
    * feat: add dependent schema for variables to constraint the 'default' attribute based on the set type
    * feat: implement schema merging for component inputs
    * feat: use ProviderReferences in dependent body schema for stack component provider keys

--- schema/stacks/deploy_schema.go
+++ schema/stacks/deploy_schema.go
@@ -9,13 +9,6 @@ import (
 	stack_1_9 "github.com/hashicorp/terraform-schema/internal/schema/stacks/1.9"
 )
 
-// CoreStackSchemaForVersion finds a schema for stack configuration files
-// that is relevant for the given Terraform version.
-// It will return an error if such schema cannot be found.
-func CoreStackSchemaForVersion(v *version.Version) (*schema.BodySchema, error) {
-	return stack_1_9.StackSchema(v), nil
-}
-
 // CoreDeploySchemaForVersion finds a schema for deployment configuration files
 // that is relevant for the given Terraform version.
 // It will return an error if such schema cannot be found.
