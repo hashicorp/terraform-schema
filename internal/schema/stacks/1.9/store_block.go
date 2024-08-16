a1ef4155cc6ef2dc5b906326200881c36a3535a0
Ansgar Mertens <ansgar@hashicorp.com>
2024-08-16 19:25:04 +0200

    refactor: use a specific example for the varset id

--- internal/schema/stacks/1.9/store_block.go
+++ internal/schema/stacks/1.9/store_block.go
@@ -51,7 +51,7 @@ func storeBlockSchema() *schema.BlockSchema {
 					"id": {
 						IsRequired:  true,
 						Constraint:  schema.LiteralType{Type: cty.String},
-						Description: lang.Markdown("The id of the varset. In the form of 'varset-nnnnnnnnnnnnnnnn'."),
+						Description: lang.Markdown("The id of the varset. In the form of 'varset-QKpocVOC3uQQxVrF'."),
 					},
 				},
 				AnyAttribute: &schema.AttributeSchema{
