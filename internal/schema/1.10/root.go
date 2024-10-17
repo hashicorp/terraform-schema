905883d05a86a397614c81b14f81863e601c4a23
Radek Simko <radeksimko@users.noreply.github.com>
2024-10-17 08:31:59 +0100

    (TFECO-7725) schema additions for ephemeral values (#394)
    * schema additions for ephemeral values
    * add description for ephemeral attribute, also add it to stack outputs
    * add sensitive to Stack outputs
    
    ---------
    
    Co-authored-by: Ansgar Mertens <ansgar@hashicorp.com>

---
+++ internal/schema/1.10/root.go
@@ -0,0 +1,30 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import (
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
+	"github.com/zclconf/go-cty/cty"
+
+	v1_9_mod "github.com/hashicorp/terraform-schema/internal/schema/1.9"
+)
+
+func ModuleSchema(v *version.Version) *schema.BodySchema {
+	bs := v1_9_mod.ModuleSchema(v)
+
+	bs.Blocks["variable"].Body.Attributes["ephemeral"] = &schema.AttributeSchema{
+		IsOptional:  true,
+		Constraint:  schema.LiteralType{Type: cty.Bool},
+		Description: lang.PlainText("Whether the value is ephemeral and should not be persisted in the state"),
+	}
+	bs.Blocks["output"].Body.Attributes["ephemeral"] = &schema.AttributeSchema{
+		IsOptional:  true,
+		Constraint:  schema.LiteralType{Type: cty.Bool},
+		Description: lang.PlainText("Whether the value is ephemeral and should not be persisted in the state"),
+	}
+
+	return bs
+}
--- internal/schema/stacks/1.9/output_block.go
+++ internal/schema/stacks/1.9/output_block.go
@@ -48,6 +48,17 @@ func outputBlockSchema() *schema.BlockSchema {
 					IsRequired:  true,
 					Description: lang.PlainText("Type of the output value"),
 				},
+				"sensitive": {
+					Constraint:   schema.LiteralType{Type: cty.Bool},
+					DefaultValue: schema.DefaultValue{Value: cty.False},
+					IsOptional:   true,
+					Description:  lang.Markdown("Whether the output contains sensitive material and should be hidden in the UI"),
+				},
+				"ephemeral": {
+					Constraint:  schema.LiteralType{Type: cty.Bool},
+					IsOptional:  true,
+					Description: lang.PlainText("Whether the value is ephemeral and should not be persisted in the state"),
+				},
 			},
 		},
 	}
