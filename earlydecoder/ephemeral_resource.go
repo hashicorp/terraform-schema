c7c7eca38335af9a341052c252f4652c7d47fffc
Ansgar Mertens <ansgar@hashicorp.com>
2024-10-23 13:30:01 +0200

    fix: prefix key for ephemeral resources

--- earlydecoder/ephemeral_resource.go
+++ earlydecoder/ephemeral_resource.go
@@ -18,5 +18,5 @@ type ephemeralResource struct {
 // MapKey returns a string that can be used to uniquely identify the receiver
 // in a map[string]*ephemeralResource.
 func (r *ephemeralResource) MapKey() string {
-	return fmt.Sprintf("%s.%s", r.Type, r.Name)
+	return fmt.Sprintf("ephemeral.%s.%s", r.Type, r.Name)
 }
