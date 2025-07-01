f0e7c5540630a968d0b20c638fdcc6186f88533c
Anubhav Goel <replyag96@gmail.com>
2025-07-01 17:24:51 +0530

    feat(tfsearch): TF-26847 TF-27259: Added: static tfsearch schemas

---
+++ schema/search/search_schema_merge.go
@@ -0,0 +1,98 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import (
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
+	tfaddr "github.com/hashicorp/terraform-registry-address"
+	tfschema "github.com/hashicorp/terraform-schema/schema"
+	tfsearch "github.com/hashicorp/terraform-schema/search"
+)
+
+type SearchSchemaMerger struct {
+	coreSchema  *schema.BodySchema
+	stateReader StateReader
+}
+
+// StateReader exposes a set of methods to read data from the internal language server state
+type StateReader interface {
+	// ProviderSchema returns the schema for a provider we have stored in memory. The can come
+	// from different sources.
+	ProviderSchema(modPath string, addr tfaddr.Provider, vc version.Constraints) (*tfschema.ProviderSchema, error)
+}
+
+func NewSearchSchemaMerger(coreSchema *schema.BodySchema) *SearchSchemaMerger {
+	return &SearchSchemaMerger{
+		coreSchema: coreSchema,
+	}
+}
+
+func (m *SearchSchemaMerger) SetStateReader(mr StateReader) {
+	m.stateReader = mr
+}
+
+func (m *SearchSchemaMerger) SchemaForSearch(meta *tfsearch.Meta) (*schema.BodySchema, error) {
+	if m.coreSchema == nil {
+		return nil, tfschema.CoreSchemaRequiredErr{}
+	}
+
+	if meta == nil {
+		return m.coreSchema, nil
+	}
+
+	if m.stateReader == nil {
+		return m.coreSchema, nil
+	}
+
+	mergedSchema := m.coreSchema.Copy()
+
+	if mergedSchema.Blocks["provider"].DependentBody == nil {
+		mergedSchema.Blocks["provider"].DependentBody = make(map[schema.SchemaKey]*schema.BodySchema)
+	}
+
+	if mergedSchema.Blocks["list"].DependentBody == nil {
+		mergedSchema.Blocks["list"].DependentBody = make(map[schema.SchemaKey]*schema.BodySchema)
+	}
+
+	if _, ok := mergedSchema.Blocks["variable"]; ok {
+		mergedSchema.Blocks["variable"].Labels = []*schema.LabelSchema{
+			{
+				Name:        "name",
+				IsDepKey:    true,
+				Description: lang.PlainText("Variable name"),
+			},
+		}
+		mergedSchema.Blocks["variable"].DependentBody = variableDependentBody(meta.Variables)
+	}
+
+	// TODO merge provider - source them from the Terraform module meta requirements TF-27288
+	// TODO merge list config - source them from the Terraform module meta requirements TF-27260
+
+	return mergedSchema, nil
+}
+
+func variableDependentBody(vars map[string]tfsearch.Variable) map[schema.SchemaKey]*schema.BodySchema {
+	depBodies := make(map[schema.SchemaKey]*schema.BodySchema)
+
+	for name, mVar := range vars {
+		depKeys := schema.DependencyKeys{
+			Labels: []schema.LabelDependent{
+				{Index: 0, Value: name},
+			},
+		}
+		depBodies[schema.NewSchemaKey(depKeys)] = &schema.BodySchema{
+			Attributes: map[string]*schema.AttributeSchema{
+				"default": {
+					Constraint:  schema.LiteralType{Type: mVar.Type},
+					IsOptional:  true,
+					Description: lang.Markdown("Default value to use when variable is not explicitly set"),
+				},
+			},
+		}
+	}
+
+	return depBodies
+}
