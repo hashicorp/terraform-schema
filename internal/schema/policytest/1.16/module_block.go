ef3315125437c917c2e6824b2547dd831677bddf
sunnyhashi <sunny.wahane@hashicorp.com>
2026-02-20 10:42:27 +0530

    Update module, provider and resource block under policytest

--- internal/schema/policytest/1.16/module_block.go
+++ internal/schema/policytest/1.16/module_block.go
@@ -28,19 +28,19 @@ func moduleBlockSchema() *schema.BlockSchema {
 		SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Module},
 		Labels: []*schema.LabelSchema{
 			{
-				Name:                   "type",
+				Name:                   "module_source",
 				SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Type, lang.TokenModifierDependent},
 				Description:            lang.PlainText("Module Source"),
 				IsDepKey:               true,
 				Completable:            true,
 			},
 			{
-				Name:                   "name",
+				Name:                   "test_case_name",
 				SemanticTokenModifiers: lang.SemanticTokenModifiers{tokmod.Name},
-				Description:            lang.PlainText("Reference Name"),
+				Description:            lang.PlainText("Test case name"),
 			},
 		},
-		Description: lang.PlainText("Defines a `mock` module configuration to test policies targeting module usage."),
+		Description: lang.PlainText("Used to validate policies that govern module usage, sources, and versions"),
 		Body: &schema.BodySchema{
 			Attributes: map[string]*schema.AttributeSchema{
 				"expect_failure": {
@@ -49,13 +49,14 @@ func moduleBlockSchema() *schema.BlockSchema {
 					IsOptional:   true,
 					Description:  lang.Markdown("Expect test to fail"),
 				},
-				"attrs": {
-					Constraint: schema.Object{
-						Attributes: schema.ObjectAttributes{},
-					},
-					IsOptional:  true,
-					Description: lang.Markdown("Specify the values that should be returned for specific attributes"),
-				},
+				// Note : Not Supported at current moment but will be introduced in the future.
+				//"attrs": {
+				//	Constraint: schema.Object{
+				//		Attributes: schema.ObjectAttributes{},
+				//	},
+				//	IsOptional:  true,
+				//	Description: lang.Markdown("Mocks the input variables passed to the module"),
+				//},
 				"meta": {
 					Constraint: schema.Object{
 						Attributes: schema.ObjectAttributes{
@@ -73,8 +74,8 @@ func moduleBlockSchema() *schema.BlockSchema {
 							},
 						},
 					},
-					IsOptional:  true,
-					Description: lang.Markdown("Meta attributes of the module"),
+					IsRequired:  true,
+					Description: lang.Markdown("Mocks the static module identifiers"),
 				},
 			},
 		},
