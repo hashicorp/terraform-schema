d90d85d374db4bcd35e87282cb73ceadcfc0f7e5
sunnyhashi <sunny.wahane@hashicorp.com>
2026-02-23 12:16:51 +0530

    Add test cases for variables

--- policy/meta.go
+++ policy/meta.go
@@ -16,4 +16,5 @@ type Meta struct {
 	ResourcePolicies map[string]ResourcePolicy
 	ProviderPolicies map[string]ProviderPolicy
 	ModulePolicies   map[string]ModulePolicy
+	Variables        map[string]Variable
 }
