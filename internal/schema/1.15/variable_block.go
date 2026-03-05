856a6bfabe0131f7b35308a68aed2ecdea0ef241
Daniel Banck <daniel@dbanck.de>
2026-03-05 11:28:50 +0100

    Add const attribute to variable block in 1.15

--- internal/schema/1.15/variable_block.go
+++ internal/schema/1.15/variable_block.go
@@ -16,5 +16,12 @@ func patchVariableBlockSchema(bs *schema.BlockSchema) *schema.BlockSchema {
 		Description: lang.Markdown("Setting this value marks the variable as deprecated. The string value provided should describe the reason for deprecation and suggest an alternative. Any usage of a deprecated variable will result in a warning being emitted to the user."),
 	}
 
+	bs.Body.Attributes["const"] = &schema.AttributeSchema{
+		Constraint:   schema.LiteralType{Type: cty.Bool},
+		DefaultValue: schema.DefaultValue{Value: cty.False},
+		IsOptional:   true,
+		Description:  lang.Markdown("Whether the variable is a constant, meaning it can be used during early stages of configuration evaluation, e.g. init."),
+	}
+
 	return bs
 }
