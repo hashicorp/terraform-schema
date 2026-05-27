9fb03ca954698a92c2c6d72388ee8e51d1661106
Morris Witthein <69427027+MorrisWitthein@users.noreply.github.com>
2026-05-27 09:32:17 +0200

    feat: update terraform test schema for v1.11 (#485)
    * feat: update terraform test schema for v1.11
    
    * fix: convert state_key & override_during from BlockSchema to
    AttributeSchema and add missing occurences of override_during attribute
    
    * fix: extract override_during attribute to own file and move mock_resource/mock_data patching into mock_provider_block
    
    * fix: use Markdown instead of PlainText for override_during description

---
+++ internal/schema/tests/1.11/override_module_block.go
@@ -0,0 +1,13 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import (
+	"github.com/hashicorp/hcl-lang/schema"
+)
+
+func patchOverrideModuleBlockSchema(bs *schema.BlockSchema) *schema.BlockSchema {
+	bs.Body.Attributes["override_during"] = overrideDuringAttributeSchema()
+	return bs
+}
